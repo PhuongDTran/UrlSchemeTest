363f11ac7ac92dd1b6ea286ec5b7a9b35bab9ce2
PhuongTran <tranduyphuong2110@gmail.com>
2018-09-05 21:35:11 -0700

    custom url scheme  created through IntentFilter

--- UrlSchemeTest/UrlSchemeTest.Android/MainActivity.cs
+++ UrlSchemeTest/UrlSchemeTest.Android/MainActivity.cs
@@ -6,10 +6,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace UrlSchemeTest.Droid
 {
     [Activity(Label = "UrlSchemeTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = "urlschemetest",
+        DataHost = "testurl")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
